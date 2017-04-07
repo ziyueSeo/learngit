1c128d689f0a162e415e6b1b5ff46266a87ad555
lpc <460488013@qq.com>
2017-04-07 15:37:09 +0800

    add a 源 file

--- chengxu/shiyan/shiyan/源.cpp
+++ chengxu/shiyan/shiyan/源.cpp
@@ -2,11 +2,12 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <Cstring>
 #include <vector>
-//#include<cctype>
-//#include<iterator>
+#include <map>
 #include<algorithm>
 using namespace std;
+//分割字符串
  vector< string> split( string str,  string pattern)
 {
 	      string::size_type pos;
@@ -30,66 +31,83 @@ int main()
 {
 	string str[35];
 	vector<string> stri;
-	int count[600];//用于计数
-	int index1;
-	int index2=0;
+	int cou[600];//用于计数
+	map <string, int> rate;
+	pair<map<string, int>::iterator, bool> Insert_Pair;
 	string str1;
 	ifstream infile;
 	infile.open("E:\\xuexi\\chengxu\\shiyan\\news.txt",ios::in);
 	if (!infile.is_open()) cout << "wrong" << endl;//用于判断文件是否被正确打开，但好像不起作用
-	//stri[]初始化
+	//stri[]和cou[]初始化
 	for (int k = 0; k < 599; k++)
 	{
-		
-		count[k] = 0;
+		cou[k] = 1;
 	}
 	stri.push_back("\0");
-	//vector<string>::iterator iter;
-	//iter = stri.begin();
-	//*iter = "\0";
-	for (index1 = 0; index1 < 34; index1++)
+	for (int index1 = 0; index1 < 34; index1++)
 	{
 		getline(infile, str[index1]);
 		transform(str[index1].begin(), str[index1].end(), str[index1].begin(), toLower);
 		//ch = zhuanhuan(str[index1]);
 		vector<string> result = split(str[index1], " "  );
-		for (int i = 0; i<result.size(); i++)
+		for (int index2 = 0; index2<result.size(); index2++)
 		{
-			str1 = result[i];
+			str1 = result[index2];
 			vector<string> str2 = split(str1, ".");
 			vector<string> str3 = split(str2[0], "\"");
 			vector<string> str4 = split(str3[0], ",");
+			//如果有空字符串就跳过
 			if (str4[0].compare("\0")==0) break;
-			//*iter="\0";
-			//字符串比较并存储到stri数组中---有冲突
-			for (int j = 0; j < stri.size(); j++)
+			//将筛选之后的字符串存到stri中
+			for (int index3 = 0; index3 < stri.size(); index3++)
 			{
-				//stri.front();
-				//stri.push_back("\0");
-				if (j<stri.size() )
+				if (index3<stri.size() )
 				{
-					if (stri[j].compare(str4[0]) == 0)
-						count[j]++;
+					if (stri[index3]==str4[0])
+						cou[index3]++;
                     else 
 				        {
 					        stri.push_back(str4[0]);
-					//stri[index2]="\0";
-					//iter++;
 					        break;
 				        }
 				}
 				
 			}
-		//	stri[index2]=str4[0];//此处出错
-			//stri.push_back(str4[0]);
-			//getline(str4[0], stri[index2]);
-			//index2++;
-			//cout << stri << endl;
 		}
 	}
-	for (int i = 0; i < stri.size(); i++)
-		cout << stri[i] <<":"<<count[i]<<endl;
-	//cout << index2 << endl;
+	//字符串比较，如果前面的和后面的一样计数器加1，并且将后面的删除
+	for (int index1 = 0; index1 < stri.size(); index1++)
+	{
+		for (int index2 = index1 + 1; index2 < stri.size(); index2++)
+		{
+			if (stri[index1].compare(stri[index2]) == 0)
+			{
+				cou[index1]++;
+				stri.erase(stri.begin() + index2, stri.begin() + index2+1);
+			}
+		}
+	}
+	//排序
+	for (int index1 = 0; index1 < 10; index1++)
+	{
+		for (int in = index1+1; in < stri.size(); in++)
+		{
+			if (cou[index1] < cou[in])
+			{
+				int a = cou[in];
+				cou[in] = cou[index1];
+				cou[index1] = a;
+				string st = stri[in];
+				stri[in] = stri[index1];
+				stri[index1] = st;
+			}
+		}
+	}
+	//输出前十个
+	for (int index1 = 0; index1 < 10; index1++)
+	{
+		cout << stri[index1] << ":" << cou[index1] << endl;
+	}
 	infile.close();
     system("pause");
 		
